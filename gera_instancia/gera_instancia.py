16920dcf8e069c42dfa21e1db3a7930b51d9bdf3
Igor Luppi <igor.luppi@gmail.com>
2017-12-03 20:05:46 -0200

    Add files via upload

--- gera_instancia/gera_instancia.py
+++ gera_instancia/gera_instancia.py
@@ -1,11 +1,13 @@
 import random
+
 str1 = "["
 str2 = "["
 rangeNum = 2000
+
 for x in range(0, 5):
-    	for y in range(0, rangeNum):
+	for y in range(0, rangeNum):
 		b = random.randrange(480, 1200)
-		c = random.randrange(5, 10)
+		c = random.randrange(300, 900)
 		str1 = str1 + "["+ str(x)+", "+str(b)+"]"
 		str2 = str2 + str(c)
 		if (y < (rangeNum-1) or x < 4):
