1d5ba7988448061d618aff20686c88c8bca3046f
Igor Luppi <igor.luppi@gmail.com>
2017-12-03 19:32:57 -0200

    Add files via upload

---
+++ api.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, json, request
+from sklearn import linear_model
+
+class Fila:
+    def __init__(self, tempo_medio_inicial):
+        self.tempo_medio_inicial = tempo_medio_inicial
+        self.training_count = 0
+        self.regressor = linear_model.SGDRegressor()
+
+app = Flask(__name__)
+
+filas = {}
+
+@app.route('/prediction/<int:ID>', methods=['GET'])
+def prediction(ID):
+    fila = filas[ID]
+    if fila.training_count < 50:
+        return jsonify({
+            'status': 'ok',
+			'type': 0,
+            'data': {
+                'prediction': fila.tempo_medio_inicial
+            },
+        })
+    return jsonify({
+        'status': 'ok',
+		'type' : 1,
+        'data': {
+            'prediction': fila.regressor.predict([
+                [request.args["dia_da_semana"], request.args["hora_de_entrada"]]
+            ])
+        },
+    })
+
+@app.route('/fit/<int:ID>', methods=['POST'])
+def fit(ID):
+    fila = filas[ID]
+    fila.regressor.partial_fit([
+            [int(request.form["dia_da_semana"]), int(request.form["hora_de_entrada"])]
+        ],
+        [int(request.form["tempo_de_espera_na_fila"])]
+    )
+    fila.training_count += 1
+    return jsonify({
+        'status': 'ok',
+    })
+    
+@app.route('/register/<int:ID>', methods=['POST'])
+def register(ID):
+    print('Request data:', request.form)
+    tempo_medio_inicial = int(request.form['tempo_medio_inicial'])
+    if ID not in filas:
+        filas[ID] = Fila(tempo_medio_inicial)
+        return jsonify({
+            'status': 'ok',
+        }), 201
+    return jsonify({
+        'status': 'error',
+    })
+       
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ gera_instancia/gera_instancia.py
@@ -0,0 +1,27 @@
+import random
+str1 = "["
+str2 = "["
+rangeNum = 2000
+for x in range(0, 5):
+    	for y in range(0, rangeNum):
+		b = random.randrange(480, 1200)
+		c = random.randrange(5, 10)
+		str1 = str1 + "["+ str(x)+", "+str(b)+"]"
+		str2 = str2 + str(c)
+		if (y < (rangeNum-1) or x < 4):
+			str1 = str1 +", "
+			str2 = str2 +", "
+		
+	
+str1 = str1 +"], "
+str2 = str2 +"]"
+str3 = str1+str2
+strFinal = "reg.partial_fit("+str3+")"
+f = open('instancia.py', 'w')
+f2 = open('caso.txt', 'w')
+
+
+f2.write(strFinal)
+f.write("from sklearn import linear_model\nreg = linear_model.SGDRegressor()\n"+strFinal+"\nprint(reg.predict([[3, 3]]))\nprint('Coef:', reg.coef_)\nprint(reg.predict([[3, 3], [13, 5]]))\nprint('Coef:', reg.coef_)")
+
+
